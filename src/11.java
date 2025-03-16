e148616a7918e8fae4fd393df2c8f186c9fdee17
wildauermcloy127 <wildauermcloy127@gmail.com>
2025-03-16 21:15:54 +0000

    Add 11.java

---
+++ src/11.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class RandomCode {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        System.out.println("List: " + list);
+    }
+}
