4dcef37b2ec5291f79127518b850406cd15175b4
wildauermcloy127 <wildauermcloy127@gmail.com>
2025-03-18 22:30:43 +0000

    Add 13.java

---
+++ src/13.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Solution{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(solve(arr));
+    }
+    public static int solve(int[] arr){
+        //Write your code here
+        return 0;
+    }
+}
