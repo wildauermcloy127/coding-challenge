db6829b0e3cfc53f18f382e9678d9ac2c058d379
wildauermcloy127 <wildauermcloy127@gmail.com>
2025-04-11 13:02:46 +0000

    Add 51.java

---
+++ src/51.java
@@ -0,0 +1,5 @@
+public class CodingChallenge {
+    public static void main(String[] args) {
+        System.out.println("This is a Java code to test your understanding of basic coding.");
+    }
+}
