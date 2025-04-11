4ea48cab066622b5523ada60863a195d9137a12b
wildauermcloy127 <wildauermcloy127@gmail.com>
2025-04-11 12:32:13 +0000

    Add 35.java

---
+++ src/35.java
@@ -0,0 +1,7 @@
+public class RandomCodeGenerator {
+    public static void main(String[] args) {
+        // Generate a random number between 1 and 100
+        int randomNumber = (int)(Math.random() * 100) + 1;
+        System.out.println("Random Number: " + randomNumber);
+    }
+}
