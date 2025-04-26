6a807f32274c54531d32a513f332340f0bbbeb4d
wildauermcloy127 <wildauermcloy127@gmail.com>
2025-04-26 01:12:16 +0000

    Add 62.java

---
+++ src/62.java
@@ -0,0 +1,6 @@
+public class SimpleProgram {
+    public static void main(String[] args) {
+        // This is just an example of basic Java program.
+        System.out.println("Hello, World!");
+    }
+}
